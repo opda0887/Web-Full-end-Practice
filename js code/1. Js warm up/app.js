c270218036900ff89e7091569af76a3ade90400d
Kiwi_Lin <opda0887@gmail.com>
2023-01-11 20:11:21 +0800

    upd: 1/11 learn

---
+++ js code/1. Js warm up/app.js
@@ -0,0 +1,22 @@
+/* console.log(), alert(), prompt() */
+console.log("Hello world!");
+
+// the two below are the same
+// alert("I'm alert~");
+// window.alert("I'm window alert~");
+
+// prompt("What's your name?");
+
+
+/* assignment and declaration */
+let x = 5;
+x++;  // x = x + 1
+console.log("x =", x);
+
+const my_age = 19;
+console.log("I'm", my_age, "year's old");
+
+
+/* variables and function */
+let name = prompt("Enter your name:");
+console.log("Hello", name);
