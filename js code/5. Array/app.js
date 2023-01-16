5d485dbe5c2d4f472c513d3b2fb4113cccb3effe
Kiwi_Lin <opda0887@gmail.com>
2023-01-16 21:08:46 +0800

    upd: 1/16 learn

---
+++ js code/5. Array/app.js
@@ -0,0 +1,21 @@
+let friends = ["John", "Eddie", "Kiwi"];
+
+// index
+console.log(friends[1]);  // Eddie - will be printed
+
+// length
+console.log(friends.length);  // 3 - will be printed
+
+// push, pop
+friends.push("Harry");
+console.log(friends);
+
+friends.pop();
+console.log(friends);
+
+// shift, unshift
+friends.shift();
+console.log(friends);
+
+friends.unshift("May");
+console.log(friends);
---
+++ js code/6. function/app.js
@@ -0,0 +1,9 @@
+// declare a function
+function sayHi( name ) {
+  console.log("Hi", name);
+  console.log("Welcome to my website.");
+}
+
+// call the function
+let name = prompt("Enter your name: ")
+sayHi(name);
