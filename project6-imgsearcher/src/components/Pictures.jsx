301d1497fb5885efc7dacaee34fb3b33b9b14238
Kiwi_Lin <opda0887@gmail.com>
2023-04-16 20:12:57 +0800

    upd: add classes and icon

--- project6-imgsearcher/src/components/Pictures.jsx
+++ project6-imgsearcher/src/components/Pictures.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const Pictures = ({ data }) => {
   return (
     <div className="img-container">
-      <p>{data.photographer}</p>
+      <p className="photographer">{data.photographer}</p>
       <div className="img-container_box">
         <img src={data.src.large} alt="a image" />
       </div>
       <p>
         Download Image:{" "}
-        <a target="_blank" href={data.src.large}>
+        <a className="click_here" target="_blank" href={data.src.large}>
           Click Here
         </a>
       </p>
